fec8a8ed2a2331a3b60e2264d06ad6067523e6f9
srinjoy-26 <srinjoydev26@gmail.com>
2024-03-09 23:55:42 +0530

    created projects data file

---
+++ src/Projects.js
@@ -0,0 +1,32 @@
+export let Projects = [
+  {
+    name:"FitTrackHub",
+    des:"A fitness-guide app made using React , Tailwind CSS ,Aos, MUI , with exercise details from excerciseDB API , and appwrite used for Backend(authentication)",
+    link:"https://fittrackhub.vercel.app/",
+    img:'/images/fittrackhub.png'
+  },
+  {
+    name:"My-Portfolio",
+    des:"My fully responsive modern portfolio website , made using React , Tailwind CSS",
+    link:"https://its-srinjoy.vercel.app/",
+    img:'/images/portfolio.png'
+  },
+  {
+    name:"Coffee-landing-page",
+    des:"a Mordern coffee-themed landing page made using HTML , CSS , JS",
+    link:"https://srinjoy-26.github.io/Coffee-landing-page/",
+    img:'/images/coffee.png'
+  },
+  {
+    name:"Meme-generator",
+    des:"a meme generator using HTML,CSS,JS and memes fteched from Dad jokes API",
+    link:"https://srinjoy-26.github.io/meme-generator/",
+    img:'/images/meme.png'
+  },
+  {
+    name:"Quiz-App",
+    des:"A Quiz game built using HTML , Tailwind-css and JS.",
+    link:"https://srinjoy-26.github.io/Quiz-game/",
+    img:'/images/quiz.png'
+  }
+]
