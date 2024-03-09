7cfdc7c44c2860328f7c1029ae335a1bc52e4d6a
srinjoy-26 <srinjoydev26@gmail.com>
2024-03-09 21:48:32 +0530

    experience page done

---
+++ src/Resume.js
@@ -0,0 +1,53 @@
+export let Education = [
+  {
+    name:"Khalsa Model Senior Secondary School",
+    duration:'2011-2020',
+    des:"Completed my 10+2 from here.",
+    type:'Edu',
+    Image:'/images/school.png'
+  },{
+    name:"Maulana Abul Kalam Azad University of Technology",
+    duration:'2021-2025',
+    des:"Pursued my Btech from here",
+    type:'Edu',
+    Image:'/images/graduate.png'
+  }
+]
+
+export let Experience = [
+  {
+    name:"Frontend intern",
+    company:"BitsGate",
+    duration:'Jan 2024 - Feb 2024 ',
+    des:"worked on various projects, primarily on web based technologies like HTML , CSS, JS, TAILWIND, REACTjs",
+    type:'Exp',
+    Image:'/images/bitsgate_logo.jpg'
+  },{
+    name:"Frontend Developer Intern",
+    company:"Napft Technology",
+    duration:'Feb 2024 - Present',
+    des:"Building the frontend of a NFT Marketplace using React , Tailwind CSS.",
+    type:'Internship',
+    Image:'/images/logo.png'
+  }
+]
+
+export let Por = [
+  {
+    name:"College Clan Chief",
+    company:"IndiGG DAO",
+    duration:'Oct 2022 - Aug 2023',
+    des:"worked with a web3 gaming community.",
+    type:'Por',
+    Image:'/images/indigg.jpg'
+  },{
+    name:"Chapter Lead",
+    company:"Hack2Skill",
+    duration:'Jun 2022 - Jan 2023 ',
+    des:"Ran a Technical club at my University under Hack2skill.",
+    type:'Por',
+    Image:'/images/H2S.svg'
+  }
+]
+ 
+
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,35 +1,67 @@
 
-
+import { useState } from "react"
+import { Education } from "../Resume"
+import { Experience } from "../Resume"
+import { Por } from "../Resume"
 
 const Resume = () => {
+  let [studydata , setstudydata] = useState(Education)
+  let [exp , setexp] = useState(Experience)
   return (
-    <div className="w-[60%] mx-auto xl:h-screen pt-[120px] relative" id="timeline">
-      <div className="container bg-slate-800 p-5 w-[50%] rounded-xl relative">
-        <img src="/images/school.png" className="h-10 w-10 bg-white rounded-full absolute top-12 right-[-23px]"/>
-        <div className="text bg-white p-3 rounded-xl">
-          <h1 className="text-2xl font-semibold">Khalsa Model Senior Secondary School</h1>
-          <small className="font-semibold text-lg text-slate-500">2011-2020</small>
-          <p className="text-lg">Completed my 10+2 from here.</p>
+    <>
+<div className="flex flex-col md:flex-row justify-center ">
+  <div className=" pt-[140px] md:pl-2">
+    <h1 className="text-white text-2xl font-semibold  text-right md:w-[85%] pr-2 md:pr-0 ">Education</h1>
+    <div className="w-full md:w-[80%]  relative " id="timeline">
+      {studydata.map((x , index) => (
+        <div className="container  p-2 md:p-5 w-[80%] md:w-full rounded-xl relative" key={index}>
+        <img src={x.Image} className="h-10 w-10 bg-white rounded-full absolute top-12 right-[-23px]"/>
+      
+        <div className="text bg-gradient-to-r from-[#F8FAE5] to-[#F3F8FF] p-3 rounded-xl">
+          <h1 className="text-xl md:text-2xl font-semibold">{x.name}</h1>
+          <small className="font-semibold text-lg text-slate-500">{x.duration}</small>
+          <p className="text-lg">{x.des}</p>
         </div>
       </div>
-
-      <div className="container bg-slate-800 p-5 w-[50%] rounded-xl relative">
-        <img src="/images/school.png" className="h-10 w-10 bg-white rounded-full absolute top-12 right-[-23px]"/>
-        <div className="text bg-white p-3 rounded-xl">
-          <h1 className="text-2xl font-semibold">Khalsa Model Senior Secondary School</h1>
-          <small className="font-semibold text-lg text-slate-500">2011-2020</small>
-          <p className="text-lg">Completed my 10+2 from here.</p>
+      ))}
+     </div>
+  
+      
+  
+      <h1 className="text-white text-2xl font-semibold text-right md:w-[85%] pr-2 md:pr-0 ">Experience</h1>
+      <div className="w-full md:w-[80%] relative" id="timeline">
+      {exp.map((x , index) => (
+        <div className="container  p-2 md:p-5 w-[80%] md:w-full rounded-xl relative" key={index}>
+        <img src={x.Image} className="h-10 w-10 bg-black rounded-full absolute top-12 right-[-23px]"/>
+        
+        <div className="text bg-gradient-to-r from-[#FAF6F0] to-[#FFD28F] p-3 rounded-xl">
+          <h1 className=" text-xl md:text-2xl font-semibold">{`${x.name}(${x.company})`}</h1>
+          <small className="font-semibold text-lg text-slate-500">{x.duration}</small>
+          <p className="text-lg">{x.des}</p>
         </div>
       </div>
-      <div className="container bg-slate-800 p-5 w-[50%] rounded-xl relative">
-        <img src="/images/school.png" className="h-10 w-10 bg-white rounded-full absolute top-12 right-[-23px]"/>
-        <div className="text bg-white p-3 rounded-xl">
-          <h1 className="text-2xl font-semibold">Khalsa Model Senior Secondary School</h1>
-          <small className="font-semibold text-lg text-slate-500">2011-2020</small>
-          <p className="text-lg">Completed my 10+2 from here.</p>
+      ))}
+      </div>
+  </div>
+  
+  <div className="md:pt-[140px] ">
+  <h1 className="text-white text-2xl  font-semibold text-right md:w-[85%] pr-2 md:pr-0 ">Positions of responsibility</h1>
+  <div className="w-full md:w-[80%] relative" id="timeline">
+      {Por.map((x , index) => (
+        <div className="container  p-2 md:p-5 w-[80%] md:w-full rounded-xl relative" key={index}>
+        <img src={x.Image} className="h-10 w-10 bg-white rounded-full absolute top-12 right-[-23px]"/>
+        
+        <div className="text bg-gradient-to-r from-[#E6B9DE] to-[#EEF5FF] p-3 rounded-xl">
+          <h1 className=" text-xl md:text-2xl font-semibold">{`${x.name}(${x.company})`}</h1>
+          <small className="font-semibold text-lg text-slate-500">{x.duration}</small>
+          <p className="text-lg">{x.des}</p>
         </div>
       </div>
-    </div>
+      ))}
+      </div>
+      </div>
+</div>
+    </>
   )
 }
 
